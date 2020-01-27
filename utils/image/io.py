ecbd0949a4079fcf1b63cced818e7c1ff65988a4
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-01-27 15:20:44 -0500

    Added function to save arrays to MHD image format

--- utils/image/io.py
+++ utils/image/io.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Tuple
 
 import SimpleITK
@@ -23,3 +24,19 @@ def load_and_process_mhd_data(filename: str) -> Tuple[np.ndarray, Tuple[np.ndarr
     im_array = np.squeeze(SimpleITK.GetArrayFromImage(image))
 
     return im_array, info
+
+
+def save_as_mhd(im_array: np.ndarray, output_filepath: Path, origin=(0, 0, 0), spacing=(1, 1, 1), dtype=np.float32):
+    """ Saves an array to mhd format.
+
+    Args:
+        im_array: the 2d or 3d image.
+        output_filepath: the output filename. Must end in ".mhd".
+        origin: center of the image.
+        spacing: size of the voxels along each dimension.
+        dtype: type of data to save.
+    """
+    seg = SimpleITK.GetImageFromArray(im_array.astype(dtype))
+    seg.SetOrigin(origin)
+    seg.SetSpacing(spacing)
+    SimpleITK.WriteImage(seg, str(output_filepath))
