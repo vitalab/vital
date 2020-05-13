ef941f88dff6203dad4a5346bcc55e648e4fb60e
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-05-13 11:45:00 -0400

    Streamlined image transform API by merging similar functions

--- utils/image/transform.py
+++ utils/image/transform.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Iterable
+from typing import Tuple, Sequence
 
 import PIL
 import numpy as np
@@ -10,7 +10,7 @@ def resize_image(image: np.ndarray, size: Tuple[int, int], resample: PIL.Image =
     """Resizes the image to the specified dimensions.
 
     Args:
-        image: input image to process.
+        image: input image to process. Must be in a format supported by PIL.
         size: width and height dimensions of the processed image to output.
         resample: resampling filter to use.
 
@@ -21,51 +21,25 @@ def resize_image(image: np.ndarray, size: Tuple[int, int], resample: PIL.Image =
     return resized_image
 
 
-def resize_segmentation(segmentation: np.ndarray, size: Tuple[int, int], resample: PIL.Image = NEAREST) -> np.ndarray:
-    """Resizes the segmentation map to the specified dimensions.
+def remove_labels(segmentation: np.ndarray, labels_to_remove: Sequence[int], fill_label: int = 0) -> np.ndarray:
+    """Removes labels from the segmentation map, reassigning the affected pixels to `fill_label`.
 
     Args:
-        segmentation: segmentation map to process.
-        size: width and height dimensions of the processed segmentation map to output.
-        resample: resampling filter to use.
-
-    Returns:
-        segmentation map resized to the specified dimensions.
-    """
-    # Ensure segmentation is in a format supported by Pillow (np.uint8)
-    # to avoid possible "Cannot handle this data type" error
-    resized_segmentation = np.array(Image.fromarray(segmentation.astype(np.uint8)).resize(size, resample=resample))
-    # TOIMPROVE Apply mathematical opening to smooth the rough frontiers from the resize, if the resolution allows it
-    return resized_segmentation
-
-
-def one_hot_remove_labels(segmentation: np.ndarray, labels_to_remove: Iterable[int], fill_label: int = 0) -> np.ndarray:
-    """Removes labels from the categorical segmentation map, reassigning the affected pixels to `fill_label`.
-
-    Args:
-        segmentation: ([N], H, W, C), categorical segmentation map from which to remove labels.
-        labels_to_remove: labels to remove.
-        fill_label: label to assign to the pixels currently assigned to the labels to remove.
-
-    Returns:
-        ([N], H, W, C - len(``labels_to_remove``)), categorical segmentation map with the labels removed.
-    """
-    for label_to_remove in labels_to_remove:
-        segmentation[..., fill_label] += segmentation[..., label_to_remove]
-    segmentation = np.delete(segmentation, labels_to_remove, axis=-1)
-    return segmentation
-
-
-def remove_labels(segmentation: np.ndarray, labels_to_remove: Iterable[int], fill_label: int = 0) -> np.ndarray:
-    """Removes labels from the labelled segmentation map, reassigning the affected pixels to `fill_label`.
-
-    Args:
-        segmentation: ([N], H, W, 1), labelled segmentation map from which to remove labels.
+        segmentation: ([N], H, W, 1/C), segmentation map from which to remove labels.
         labels_to_remove: labels to remove.
         fill_label: label to assign to the pixels currently assigned to the labels to remove.
 
     Returns:
         ([N], H, W, 1), labelled segmentation map with the specified labels removed.
     """
-    segmentation[np.isin(segmentation, labels_to_remove)] = fill_label
+    segmentation = segmentation.copy()
+
+    if segmentation.shape[-1] == 1:  # If the segmentation map is labelled
+        segmentation[np.isin(segmentation, labels_to_remove)] = fill_label
+
+    else:  # the segmentation map is in one-hot format
+        for label_to_remove in labels_to_remove:
+            segmentation[..., fill_label] += segmentation[..., label_to_remove]
+        segmentation = np.delete(segmentation, labels_to_remove, axis=-1)
+
     return segmentation
