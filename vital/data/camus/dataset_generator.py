5c295309541eb281b4f526c26c354196fdb15f21
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-07-27 23:14:30 -0400

    Updated HDF5 dataset generation CLI and README documentation

--- vital/data/camus/dataset_generator.py
+++ vital/data/camus/dataset_generator.py
@@ -252,10 +252,7 @@ def main():
 
     parser = ArgumentParser()
     parser.add_argument(
-        "--data",
-        type=Path,
-        required=True,
-        help="Path to the CAMUS root directory, under which the patient directories are stored",
+        "data", type=Path, help="Path to the CAMUS root directory, under which the patient directories are stored",
     )
     parser.add_argument(
         "--output",
