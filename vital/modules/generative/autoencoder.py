4ec2a8e44988552bd3070f435bf936266b26089a
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-08-05 14:42:53 -0400

    Refactored autoencoder modules to return dicts in forward pass
    - This eases development of code compatible with both AE and VAE modules

--- vital/modules/generative/autoencoder.py
+++ vital/modules/generative/autoencoder.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Dict, Tuple
 
 from torch import Tensor, nn
 
@@ -10,6 +10,13 @@ from vital.modules.layers import reparameterize
 class Autoencoder(nn.Module):
     """Module making up a fully convolutional autoencoder."""
 
+    # Tags used as keys in the dict returned by the forward pass
+    reconstruction_tag: str = "x_hat"
+    encoding_tag: str = "z"
+
+    #:  Whether the encoder has a second head to output the ``logvar`` along with the default ``mu`` head
+    output_distribution: bool = False
+
     def __init__(
         self, image_size: Tuple[int, int], channels: int, blocks: int, init_channels: int, latent_dim: int
     ):  # noqa: D205,D212,D415
@@ -29,6 +36,7 @@ class Autoencoder(nn.Module):
             blocks=blocks,
             init_channels=init_channels,
             latent_dim=latent_dim,
+            output_distribution=self.output_distribution,
         )
         self.decoder = Decoder(
             image_size=image_size,
@@ -38,59 +46,38 @@ class Autoencoder(nn.Module):
             latent_dim=latent_dim,
         )
 
-    def forward(self, y: Tensor) -> Tuple[Tensor, Tensor]:
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
         """Defines the computation performed at every call.
 
         Args:
-            y: (N, ``channels``, H, W), Input to reconstruct.
+            x: (N, ``channels``, H, W), Input to reconstruct.
 
         Returns:
+            Dict with values:
             - (N, ``channels``, H, W), Raw, unnormalized scores for each class in the input's reconstruction.
             - (N, ``latent_dim``), Encoding of the input in the latent space.
         """
-        z = self.encoder(y)
-        return self.decoder(z), z
+        z = self.encoder(x)
+        return {self.reconstruction_tag: self.decoder(z), self.encoding_tag: z}
 
 
-class VariationalAutoencoder(nn.Module):
+class VariationalAutoencoder(Autoencoder):
     """Module making up a fully convolutional variational autoencoder."""
 
-    def __init__(
-        self, image_size: Tuple[int, int], channels: int, blocks: int, init_channels: int, latent_dim: int
-    ):  # noqa: D205,D212,D415
-        """
-        Args:
-            image_size: Size of the output segmentation groundtruth for each axis.
-            channels: Number of channels of the image to reconstruct.
-            blocks: Number of upsampling transposed convolution blocks to use.
-            init_channels: Number of output feature maps from the first layer, used to compute the number of feature
-                maps in following layers.
-            latent_dim: Number of dimensions in the latent space.
-        """
-        super().__init__()
-        self.encoder = Encoder(
-            image_size=image_size,
-            in_channels=channels,
-            blocks=blocks,
-            init_channels=init_channels,
-            latent_dim=latent_dim,
-            output_distribution=True,
-        )
-        self.decoder = Decoder(
-            image_size=image_size,
-            out_channels=channels,
-            blocks=blocks,
-            init_channels=init_channels,
-            latent_dim=latent_dim,
-        )
+    # Tags used as keys in the dict returned by the forward pass
+    distr_mean_tag: str = "mu"
+    distr_logvar_tag: str = "logvar"
 
-    def forward(self, y: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+    output_distribution = True
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
         """Defines the computation performed at every call.
 
         Args:
-            y: (N, ``channels``, H, W), Input to reconstruct.
+            x: (N, ``channels``, H, W), Input to reconstruct.
 
         Returns:
+            Dict with values:
             - (N, ``channels``, H, W), Raw, unnormalized scores for each class in the input's reconstruction.
             - (N, ``latent_dim``), Sampled encoding of the input in the latent space.
             - (N, ``latent_dim``), Mean of the predicted distribution of the input in the latent space, used to sample
@@ -98,6 +85,11 @@ class VariationalAutoencoder(nn.Module):
             - (N, ``latent_dim``), Log variance of the predicted distribution of the input in the latent space, used to
               sample ``z``.
         """
-        mu, logvar = self.encoder(y)
+        mu, logvar = self.encoder(x)
         z = reparameterize(mu, logvar)
-        return self.decoder(z), z, mu, logvar
+        return {
+            self.reconstruction_tag: self.decoder(z),
+            self.encoding_tag: z,
+            self.distr_mean_tag: mu,
+            self.distr_logvar_tag: logvar,
+        }
