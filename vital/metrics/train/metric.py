7ed1018ef2ec3c3da14f79826d40d9af50c4a86d
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-10-09 17:39:46 -0400

    Migrated metrics to new PL `Metric` API
    - Also fixed typo in module name

--- vital/metrics/train/metric.py
+++ vital/metrics/train/metric.py
@@ -1,12 +1,12 @@
-from typing import Any
+from typing import Any, Optional
 
-from pytorch_lightning.metrics import TensorMetric
-from torch import Tensor
+import torch
+from pytorch_lightning.metrics import Metric
 
-from vital.metrics.train.functionnal import differentiable_dice_score, kl_div_zmuv
+from vital.metrics.train.functional import differentiable_dice_score, kl_div_zmuv
 
 
-class DifferentiableDiceCoefficient(TensorMetric):
+class DifferentiableDiceCoefficient(Metric):
     """Computes a differentiable version of the dice coefficient."""
 
     def __init__(
@@ -15,7 +15,9 @@ class DifferentiableDiceCoefficient(TensorMetric):
         nan_score: float = 0.0,
         no_fg_score: float = 0.0,
         reduction: str = "elementwise_mean",
-        reduce_group: Any = None,
+        compute_on_step: bool = True,
+        ddp_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
     ):  # noqa: D205,D212,D415
         """
         Args:
@@ -26,56 +28,77 @@ class DifferentiableDiceCoefficient(TensorMetric):
                 Available reduction methods:
                 - ``'elementwise_mean'``: takes the mean (default)
                 - ``'none'``: no reduction will be applied
-            reduce_group: Process group to reduce metric results from DDP.
+            compute_on_step: Forward only calls ``update()`` and returns None if this is set to False.
+            ddp_sync_on_step: Synchronize metric state across processes at each ``forward()`` before returning the value
+                at the step.
+            process_group: Specify the process group on which synchronization is called.
+                Selects the entire world by default.
         """
-        super().__init__(name="differentiable_dice", reduce_group=reduce_group)
+        super().__init__(
+            compute_on_step=compute_on_step, ddp_sync_on_step=ddp_sync_on_step, process_group=process_group
+        )
         self.include_background = include_background
         self.nan_score = nan_score
         self.no_fg_score = no_fg_score
         assert reduction in ("elementwise_mean", "none")
         self.reduction = reduction
 
-    def forward(self, input: Tensor, target: Tensor) -> Tensor:
-        """Actual metric calculation.
+        self.add_state("dice_by_steps", [])
+
+    def update(self, input: torch.Tensor, target: torch.Tensor) -> None:
+        """Update state with dice over the batch.
 
         Args:
             input: (N, C, H, W), Raw, unnormalized scores for each class.
             target: (N, H, W), Groundtruth labels, where each value is 0 <= targets[i] <= C-1.
+        """
+        self.dice_by_steps += [
+            differentiable_dice_score(
+                input=input,
+                target=target,
+                bg=self.include_background,
+                nan_score=self.nan_score,
+                no_fg_score=self.no_fg_score,
+                reduction=self.reduction,
+            )
+        ]
+
+    def compute(self) -> torch.Tensor:
+        """Reduces dice over state.
 
         Return:
             (1,) or (C,), Calculated dice coefficient, averaged or by labels.
         """
-        return differentiable_dice_score(
-            input=input,
-            target=target,
-            bg=self.include_background,
-            nan_score=self.nan_score,
-            no_fg_score=self.no_fg_score,
-            reduction=self.reduction,
-        )
+        return torch.mean(torch.stack(self.dice_by_steps), 0)
 
 
-class KlDivergenceToZeroMeanUnitVariance(TensorMetric):
+class KlDivergenceToZeroMeanUnitVariance(Metric):
     """Computes the KL divergence between a specified distribution and a N(0,1) Gaussian distribution.
 
     It is the standard loss to use for the reparametrization trick when training a variational autoencoder.
     """
 
-    def __init__(self, reduce_group: Any = None):  # noqa: D205,D212,D415
-        """
-        Args:
-            reduce_group: Process group to reduce metric results from DDP.
-        """
-        super().__init__(name="kl_div_zmuv", reduce_group=reduce_group)
+    def __init__(
+        self, compute_on_step: bool = True, ddp_sync_on_step: bool = False, process_group: Optional[Any] = None
+    ):
+        super().__init__(
+            compute_on_step=compute_on_step, ddp_sync_on_step=ddp_sync_on_step, process_group=process_group
+        )
+        self.add_state("kl_div_by_steps", [])
 
-    def forward(self, mu: Tensor, logvar: Tensor) -> Tensor:
-        """Actual metric calculation.
+    def update(self, mu: torch.Tensor, logvar: torch.Tensor) -> None:
+        """Update state with KL divergence for the batch.
 
         Args:
             mu: (N, Z), Mean of the distribution to compare to N(0,1).
             logvar: (N, Z) Log variance of the distribution to compare to N(0,1).
+        """
+        self.kl_div_by_steps += [kl_div_zmuv(mu, logvar)]
+
+    def compute(self) -> torch.Tensor:
+        """Reduces KL divergence over state.
 
         Returns:
             (1,), KL divergence term of the VAE's loss.
         """
-        return kl_div_zmuv(mu, logvar)
+        return torch.mean(torch.stack(self.kl_div_by_steps), 0)
