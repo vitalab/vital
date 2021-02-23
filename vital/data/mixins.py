f3e13c5f06245262e0640110dafc1dbfe6b23f8c
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2021-02-23 16:40:13 -0500

    Refactored `StructuredDataMixin` and CAMUS impl. to be more generic w.r.t. subsets

--- vital/data/mixins.py
+++ vital/data/mixins.py
@@ -1,31 +1,21 @@
+from abc import abstractmethod
 from typing import List
 
+from vital.data.config import Subset
 from vital.systems.system import SystemDataManagerMixin
 
 
 class StructuredDataMixin(SystemDataManagerMixin):
-    """``SystemDataManagerMixin`` mixin for datasets where data has more structure than only the target labels."""
+    """``SystemDataManagerMixin`` for datasets where data has more structure than only the target labels."""
 
-    def train_group_ids(self, *args, **kwargs) -> List[str]:
-        """Lists the IDs of the different groups/clusters samples in the training data can belong to.
-
-        Args:
-            *args: Positional arguments that parameterize the requested data structure.
-            **kwargs: Keyword arguments that parameterize the requested data structure.
-
-        Returns:
-            IDs of the different groups/clusters samples in the training data can belong to.
-        """
-        raise NotImplementedError
-
-    def val_group_ids(self, *args, **kwargs) -> List[str]:
-        """Lists the IDs of the different groups/clusters samples in the validation data can belong to.
+    @abstractmethod
+    def group_ids(self, subset: Subset, *args, **kwargs) -> List[str]:
+        """Lists the IDs of the different groups/clusters samples in the data can belong to.
 
         Args:
             *args: Positional arguments that parameterize the requested data structure.
             **kwargs: Keyword arguments that parameterize the requested data structure.
 
         Returns:
-            IDs of the different groups/clusters samples in the validation data can belong to.
+            IDs of the different groups/clusters samples in the data can belong to.
         """
-        raise NotImplementedError
