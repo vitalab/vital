d1935bed6f242905d20e3baacb3bbbcaff698e44
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2022-05-17 17:32:27 +0200

    Added `SegmentationMetrics` results processor + generic CAMUS options processor
    Standardized specification of CAMUS labels in Hydra configs

---
+++ vital/results/camus/__init__.py
@@ -0,0 +1,29 @@
+from abc import ABC
+from argparse import ArgumentParser
+
+from vital.data.camus.config import Label
+
+
+class CamusResultsProcessor(ABC):
+    """Class that encapsulates the handling of CAMUS-specific arguments for results processors' CLI."""
+
+    @classmethod
+    def add_labels_args(cls, parser: ArgumentParser) -> ArgumentParser:
+        """Adds arguments for handling CAMUS-specific arguments to a parser.
+
+        Args:
+           parser: Parser object for which to add arguments CAMUS-specific arguments .
+
+        Returns:
+            Parser object with support for CAMUS-specific arguments.
+        """
+        parser.add_argument(
+            "--labels",
+            type=Label.from_name,
+            default=list(Label),
+            nargs="+",
+            choices=list(Label),
+            help="Labels of the classes included in the segmentations. By default, considers that all class labels "
+            "provided in the dataset are included",
+        )
+        return parser
