bd7d0c7f18f48c1e5be1aa595b31e1dc4a091547
Nathan Painchaud <23144457+nathanpainchaud@users.noreply.github.com>
2022-07-12 16:54:29 +0200

    Fixes and improvements for anatomical data augmentation (#60)
    * Update reference to deprecated image I/O function
    
    * Use Lightning `TrainerFn` enum instead of hard-coded string
    
    * Improve configuration of how to extract item to encode from batch data

--- vital/tasks/utils/autoencoder.py
+++ vital/tasks/utils/autoencoder.py
@@ -3,27 +3,32 @@ import itertools
 import numpy as np
 import torch
 from pytorch_lightning import LightningDataModule
+from pytorch_lightning.trainer.states import TrainerFn
 from tqdm.auto import tqdm
 
-from vital.data.config import Tags
 from vital.tasks.autoencoder import SegmentationAutoencoderTask
 
 
 def encode_dataset(
-    autoencoder: SegmentationAutoencoderTask, datamodule: LightningDataModule, progress_bar: bool = False
+    autoencoder: SegmentationAutoencoderTask,
+    datamodule: LightningDataModule,
+    segmentation_data_tag: str = None,
+    progress_bar: bool = False,
 ) -> np.ndarray:
     """Encodes masks from the train and val sets of a dataset in the latent space learned by an autoencoder model.
 
     Args:
         autoencoder: Autoencoder model used to encode masks in a latent space.
         datamodule: Abstraction of the dataset to encode, allowing to access both the training and validation sets.
+        segmentation_data_tag: Key to locate the data to encode from all the data returned in a batch. If not provided,
+            defaults to the tag of the dataset the autoencoder was trained on.
         progress_bar: Whether to display a progress bar for the encoding of the samples.
 
     Returns:
         Array of training and validation samples encoded in the latent space.
     """
     # Setup the datamodule used to get the data points to encode in the latent space
-    datamodule.setup(stage="fit")
+    datamodule.setup(stage=TrainerFn.FITTING)
     train_dataloader, val_dataloader = datamodule.train_dataloader(), datamodule.val_dataloader()
     data = itertools.chain(train_dataloader, val_dataloader)
     num_batches = len(train_dataloader) + len(val_dataloader)
@@ -32,8 +37,9 @@ def encode_dataset(
         data = tqdm(data, desc="Encoding groundtruths", total=num_batches, unit="batch")
 
     # Encode training and validation groundtruths in the latent space
+    seg_data_tag = autoencoder.hparams.segmentation_data_tag if not segmentation_data_tag else segmentation_data_tag
     with torch.no_grad():
-        dataset_samples = [autoencoder(batch[Tags.gt], task="encode").cpu() for batch in data]
+        dataset_samples = [autoencoder(batch[seg_data_tag], task="encode").cpu() for batch in data]
     dataset_samples = torch.cat(dataset_samples).numpy()
 
     return dataset_samples
