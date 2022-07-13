fa98057ae2416a6db76b73bdcff0fc189a3cccf9
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2022-07-13 17:38:20 +0200

    Added CAMUS temporal metrics

---
+++ vital/utils/norm.py
@@ -0,0 +1,21 @@
+from typing import Tuple
+
+import numpy as np
+
+
+def scale(data: np.ndarray, bounds: Tuple[float, float] = None) -> np.ndarray:
+    """Standardizes data w.r.t. predefined min/max bounds, if provided, and its own min/max otherwise.
+
+    Args:
+        data: Data to scale.
+        bounds: Prior min and max bounds to use to scale the data.
+
+    Returns:
+        Data scaled w.r.t. the predefined or computed bounds.
+    """
+    # If no prior min/max bounds for the data are provided, compute them from its values
+    if bounds is None:
+        min, max = data.min(), data.max()
+    else:
+        min, max = bounds
+    return (data - min) / (max - min)
