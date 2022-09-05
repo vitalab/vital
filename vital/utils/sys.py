97976733230b0c3a8d481d168731a1a623e78310
Nathan Painchaud <23144457+nathanpainchaud@users.noreply.github.com>
2022-09-06 01:17:02 +0200

    Migrate from `gpus` flag (to be deprecated in PL 1.7) to `accelerator` and `devices` (#78)

--- vital/utils/sys.py
+++ vital/utils/sys.py
@@ -1,7 +1,6 @@
 import os
 
 import numpy as np
-import torch
 from omegaconf import OmegaConf
 
 from vital import get_vital_root
@@ -9,7 +8,6 @@ from vital import get_vital_root
 
 def register_omegaconf_resolvers() -> None:
     """Registers various OmegaConf resolvers useful to query system info."""
-    OmegaConf.register_new_resolver("sys.gpus", lambda x=None: int(torch.cuda.is_available()))
     OmegaConf.register_new_resolver("sys.num_workers", lambda x=None: os.cpu_count() - 1)
     OmegaConf.register_new_resolver("sys.getcwd", lambda x=None: os.getcwd())
     OmegaConf.register_new_resolver("sys.eps.np", lambda dtype: np.finfo(np.dtype(dtype)).eps)
