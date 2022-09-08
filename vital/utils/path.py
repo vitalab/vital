5eca3e8c75b3af2da1172cb5f4c74c5710c27a0d
Nathan Painchaud <23144457+nathanpainchaud@users.noreply.github.com>
2022-09-09 00:52:50 +0200

    Improve generic tools for handling file extensions (#81)
    - Function to make sure a string is a file extension
    - Function to test if an image format is supported by SimpleITK

--- vital/utils/path.py
+++ vital/utils/path.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 
 
+def as_file_extension(extension: str) -> str:
+    """Ensures the input string is a valid suffix, by adding a leading dot if necessary.
+
+    Args:
+        extension: String to ensure is a file extension.
+
+    Returns:
+        String, converted to a valid suffix by adding a leading dot if necessary.
+    """
+    if not extension.startswith("."):
+        extension = "." + extension
+    return extension
+
+
 def remove_suffixes(filename: Path) -> Path:
     """Removes all the suffixes from `filename`, unlike `stem` which only removes the last suffix.
 
