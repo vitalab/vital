ee90a8bfca909e0399e0609450d282a59c9939f3
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2022-09-09 18:45:50 +0200

    Remove broken function to detect SimpleITK's support for image I/O formats

--- vital/utils/image/io.py
+++ vital/utils/image/io.py
@@ -1,37 +1,9 @@
 from pathlib import Path
-from typing import Any, Dict, Literal, Tuple, Union
+from typing import Any, Dict, Tuple, Union
 
 import numpy as np
 import SimpleITK as sitk
 from PIL import Image, ImageSequence
-
-
-def check_image_io_format_support(format: str, io_mode: Literal["read", "write"], raise_err: bool = False) -> bool:
-    """Checks if a specific `format` image extension is supported by SimpleITK image for either read/write operations.
-
-    Args:
-        format: Image format.
-        io_mode: Target I/O mode for which to test if the format is supported by SimpleITK.
-        raise_err: Whether to raise an error if the image format is not supported by SimpleITK.
-
-    Returns:
-        `True` if the image format is supported by SimpleITK for the target I/O mode, `False` otherwise.
-    """
-    match io_mode:
-        case "read":
-            image_io_formats = sitk.ImageFileReader().GetRegisteredImageIOs()
-        case "write":
-            image_io_formats = sitk.ImageFileWriter().GetRegisteredImageIOs()
-        case _:
-            raise ValueError(f"Unexpected value for 'io_mode': {io_mode}. Use one of: ['read', 'write'].")
-
-    is_format_supported = format not in image_io_formats
-    if raise_err and not is_format_supported:
-        raise RuntimeError(
-            f"Image format requested not supported by SimpleITK backend. Use one of the following supported "
-            f"'{io_mode}' format instead: {image_io_formats}."
-        )
-    return is_format_supported
 
 
 def sitk_load(filepath: Union[str, Path]) -> Tuple[np.ndarray, Dict[str, Any]]:
