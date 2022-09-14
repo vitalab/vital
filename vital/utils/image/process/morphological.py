4a2911581cdc35fcfe0f7ba193dded21302b9720
Nathan Painchaud <23144457+nathanpainchaud@users.noreply.github.com>
2022-09-14 23:57:28 +0200

    Refactor existing post-processing operations + add new autoencoder post-processing (#63)
    * Refactor image post-processing operations into distinct package
    
    * Add generic autoencoder post-processing + autoencoder filtering each latent dimension independently

--- vital/utils/image/process/morphological.py
+++ vital/utils/image/process/morphological.py
@@ -1,31 +1,13 @@
 from abc import abstractmethod
-from typing import Any, Dict, Mapping, Sequence, Union
+from typing import Sequence
 
 import numpy as np
 from scipy import ndimage
-from scipy.ndimage import gaussian_filter
 
 from vital.data.config import SemanticStructureId
 from vital.utils.decorators import batch_function
 from vital.utils.format.native import flatten
-from vital.utils.format.numpy import to_onehot
-
-
-class PostProcessor:
-    """Base class for handling the post-processing of batch of images."""
-
-    @abstractmethod
-    def __call__(self, batch: np.ndarray) -> Union[np.ndarray, Dict[str, Any]]:
-        """Processes a batch of images.
-
-        Args:
-            batch: (N, H, W), Batch of 2D images to post-process.
-
-        Returns:
-            (N, H, W), Processed batch of 2D images maps
-            or
-            Dict containing at least the processed batch of images under the `Tags.post_pred` key.
-        """
+from vital.utils.image.process import PostProcessor
 
 
 class PostProcessor2DStructure(PostProcessor):
@@ -155,53 +137,3 @@ class Post2DFillInterHoles:
         post_img[inter_holes_mask] = self._fill_label
 
         return post_img
-
-
-class PostGaussianFilter(PostProcessor):
-    """Post-processing that applies a gaussian filter along specific dimensions of the segmentation maps."""
-
-    def __init__(
-        self,
-        sigmas: Mapping[int, float],
-        sigmas_as_ratio: bool = False,
-        extend_modes: Mapping[int, str] = None,
-        do_argmax: bool = True,
-    ):
-        """Initializes class instance.
-
-        Args:
-            sigmas: Mapping between dimension(s) along which to apply the filter, and the standard deviation to use for
-                that dimension's Gaussian kernel.
-            sigmas_as_ratio: Whether to interpret `sigmas` relative to the length of the input along that dimension, so
-                that the true standard deviation for a given `dim` becomes `sigma * input.shape[dim]`.
-            extend_modes: Mapping between dimension(s) along which to apply the filter, and the mode to use to extend
-                the input when the filter overlaps a border.
-            do_argmax: If `True`, pass the filter's result through an argmax to restore "hard" class labels. Otherwise,
-                returns directly the "soft" class probabilities computed by the Gaussian filter, in a new dimension
-                added last.
-        """
-        self._kernel_stddevs = sigmas
-        self._scale_stddevs = sigmas_as_ratio
-        self._extend_modes = extend_modes if extend_modes else {}
-        self._do_argmax = do_argmax
-
-    def __call__(self, seg: np.ndarray, **kwargs) -> np.ndarray:
-        """Applies a gaussian filter along specific dimensions of the segmentation maps.
-
-        Args:
-            seg: (N, H, W), Segmentation to process.
-            **kwargs: Capture non-used parameters to get a callable API compatible with similar callables.
-
-        Returns:
-            (N, H, W, [C]), Processed segmentation, with an additional last dimension for the probability of each class
-            if `do_argmax=False`.
-        """
-        onehot_seg = to_onehot(seg)
-        sigmas = np.array([self._kernel_stddevs.get(dim, 0) for dim in range(onehot_seg.ndim)])
-        if self._scale_stddevs:
-            sigmas = sigmas * onehot_seg.shape
-        modes = [self._extend_modes.get(dim, "reflect") for dim in range(onehot_seg.ndim)]
-        filtered_seg = gaussian_filter(onehot_seg, sigmas, output=float, mode=modes)
-        if self._do_argmax:
-            filtered_seg = filtered_seg.argmax(axis=-1).astype(seg.dtype)
-        return filtered_seg
