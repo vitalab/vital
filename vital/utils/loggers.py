15c8e6a2fd4bb3d80c2b5acc5a9c5f297aef2c5b
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2022-11-24 22:18:29 +0100

    Add generic function to log figure to Lightning logger

---
+++ vital/utils/loggers.py
@@ -0,0 +1,22 @@
+from pytorch_lightning.loggers import CometLogger, Logger
+from pytorch_lightning.loggers.logger import DummyLogger
+
+
+def log_figure(logger: Logger, **kwargs) -> None:
+    """Logs a figure using a Lightning logger.
+
+    Args:
+        logger: Logger with which to log the figure.
+        **kwargs: Additional parameters to pass along to the figure-logging function for the specific logger being
+            used (e.g. Tensorboard, Comet, etc.).
+    """
+    match logger:
+        case DummyLogger():
+            pass
+        case CometLogger():
+            if kwargs.get("step") == 0:
+                # Since figures logged at step 0 are not shown by Comet, workaround by assigning step 1 in this case
+                kwargs["step"] = 1
+            logger.experiment.log_figure(**kwargs)
+        case _:
+            raise NotImplementedError(f"Logging figures not implemented for '{logger.__class__.__name__}' logger.")
